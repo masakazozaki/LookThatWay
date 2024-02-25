0ac524136316b1c053451e2d542e3f73d14e8542
Masakaz Ozaki <masakaz@ieee.org>
2024-02-25 23:57:41 +0900

    feat: face sticker

--- Look That Way.swiftpm/ImageStickerPlacer.swift
+++ Look That Way.swiftpm/ImageStickerPlacer.swift
@@ -12,21 +12,14 @@ extension UIImage {
         let renderer = UIGraphicsImageRenderer(size: self.size)
 
         let stickeredImage = renderer.image { context in
-            // 元の画像を描画
             self.draw(at: .zero)
-
-            // スタンプ画像をランダムに選択
-            let stickerName = "\((1...3).randomElement()!)" // スタンプ画像名
+            let stickerName = "\((1...10).randomElement()!)"
             if let stickerImage = UIImage(named: stickerName) {
-                let randomSize = CGFloat((200...400).randomElement()!) // ランダムなサイズ
-                let randomAngle = CGFloat(((-15)...15).randomElement()!) // ランダムな角度
+                let randomSize = CGFloat((200...400).randomElement()!)
+                let randomAngle = CGFloat(((-15)...15).randomElement()!)
                 let randomX = CGFloat.random(in: 0...(self.size.width - randomSize))
                 let randomY = CGFloat.random(in: 0...(self.size.height - randomSize))
-
-                // コンテキストの保存
                 context.cgContext.saveGState()
-
-                // 回転と配置のためのトランスフォーム
                 context.cgContext.translateBy(x: randomX + randomSize / 2, y: randomY + randomSize / 2)
                 context.cgContext.rotate(by: randomAngle * CGFloat.pi / 180)
                 context.cgContext.translateBy(x: -randomSize / 2, y: -randomSize / 2)
